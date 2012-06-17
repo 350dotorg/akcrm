347b2feacffe77d20991388915528a49cb5281b1
Robert Marianski <rob@marianski.com>
2012-06-17 11:45:19 -0400

    add stable sort by core user id

--- search/query.py
+++ search/query.py
@@ -178,6 +178,5 @@ def user_sql(spec):
     # manually join with core_userfield and phone
     joins = ' '.join(spec['joins'])
     filters = spec['filters']
-    return (('SELECT distinct cu.* FROM core_user cu %s WHERE %s' %
-             (joins, filters)),
-            spec['parameters'])
+    query = 'SELECT distinct cu.* FROM core_user cu %s WHERE %s ORDER BY cu.id'
+    return (query % (joins, filters), spec['parameters'])
