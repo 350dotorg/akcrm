302b835e4c8dea04b8a69d182cddbdd8472a8639
Ethan Jucovy <ejucovy@gmail.com>
2012-07-17 09:25:58 -0400

    use full import path

--- search/admin.py
+++ search/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from search.models import SearchField
-from search.models import SearchQuery
+from akcrm.search.models import SearchField
+from akcrm.search.models import SearchQuery
 
 admin.site.register(SearchField)
 admin.site.register(SearchQuery)
