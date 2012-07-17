5c0ec7380213caae58bcc4a2d1b8b03a181834b5
Robert Marianski <rob@marianski.com>
2012-07-17 00:24:27 -0400

    add validation for duplicate slugs

--- crm/forms.py
+++ crm/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from akcrm.crm.models import ContactRecord
+from akcrm.search.models import SearchQuery
 
 class ContactForm(forms.ModelForm):
 
@@ -17,3 +18,16 @@ class SearchSaveForm(forms.Form):
     title = forms.CharField(max_length=128)
     description = forms.CharField(widget=forms.Textarea(), required=False)
     querystring = forms.CharField()
+
+    def clean(self):
+        cleaned_data = super(SearchSaveForm, self).clean()
+        slug = cleaned_data['slug']
+        try:
+            SearchQuery.objects.get(slug=slug)
+        except SearchQuery.DoesNotExist:
+            pass
+        else:
+            msg = 'Slug already exists. Please choose another.'
+            self._errors['slug'] = self.error_class([msg])
+            del cleaned_data['slug']
+        return cleaned_data
