dd41d1dc0240bd98ea224484b1b1b66ac260e358
Robert Marianski <rob@marianski.com>
2013-02-27 23:27:21 -0500

    add function to issue ak http queries

--- actionkit/rest.py
+++ actionkit/rest.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+import json
 import requests
 from requests.auth import HTTPBasicAuth
 
@@ -20,3 +21,18 @@ def create_action(page_id, user_id):
 
     actionkit = get_client()
     return actionkit.act({'page': page_name, 'id': user_id})
+
+def query(query, timeout=10):
+    host = settings.ACTIONKIT_API_HOST
+    if not host.startswith("https"):
+        host = "https://" + host
+
+    url = "%s/rest/v1/report/run/sql/" % host
+    data = json.dumps(dict(query=query))
+    resp = requests.post(url,
+        auth=HTTPBasicAuth(
+            settings.ACTIONKIT_API_USER, settings.ACTIONKIT_API_PASSWORD),
+        headers={'content-type': 'application/json'},
+        timeout=timeout,
+        data=data)
+    return resp
