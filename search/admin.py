62bc1fb15e7a889d52afb14ac7af5832a51520bd
root <root@trac.350.org>
2013-03-15 20:48:02 +0000

    admin

--- search/admin.py
+++ search/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from akcrm.search.models import SearchField
 from akcrm.search.models import SearchQuery
+from akcrm.search.models import ActiveReport
 
 admin.site.register(SearchField)
 admin.site.register(SearchQuery)
+admin.site.register(ActiveReport)
